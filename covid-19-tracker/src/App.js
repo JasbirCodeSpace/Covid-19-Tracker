b5c69c9120c76f4f7ef32a6ceaa38aab1e86a65b
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-26 16:08:49 +0530

    Added countries table functionality

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -8,13 +8,15 @@ import {
 } from "@material-ui/core";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
+import Table from "./Table";
+import { sortData } from "./util";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
-
+  const [tableData, setTableData] = useState([]);
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
       .then((response) => response.json())
@@ -32,6 +34,8 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso3,
           }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -95,7 +99,8 @@ function App() {
       </div>
       <Card className="app__right">
         <CardContent>
-          <h1>Cases by country</h1>
+          <h3>Cases by country</h3>
+          <Table countries={tableData} />
         </CardContent>
       </Card>
       {/* Table */}
---
+++ covid-19-tracker/src/util.js
@@ -0,0 +1,4 @@
+export const sortData = (data) => {
+  const sortedData = [...data];
+  return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+};
