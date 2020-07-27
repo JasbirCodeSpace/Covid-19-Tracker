4fceb876fa82200ead7a0017a1366cf1d96b829e
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-27 16:13:04 +0530

    InfoBox styling and props handle done

--- covid-19-tracker/src/InfoBox.js
+++ covid-19-tracker/src/InfoBox.js
@@ -1,16 +1,39 @@
-import React from 'react'
-import {Card, CardContent, Typography} from "@material-ui/core"
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
 
-function InfoBox({ title, cases, total}) {
-    return (
-        <Card className="infoBox">
-            <CardContent>
-                <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
-                <h1 className="infoBox__cases">{cases}</h1>
-                <Typography className="infoBox__total">{total} Total</Typography>
-            </CardContent>
-        </Card>
-    )
+function InfoBox({
+  title,
+  cases,
+  isRed,
+  isOrange,
+  isGreen,
+  active,
+  total,
+  ...props
+}) {
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      } ${isOrange && "infoBox--orange"} ${isGreen && "infoBox--green"}`}
+    >
+      <CardContent>
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
+        <h2
+          className={`infoBox__cases ${
+            !isRed && !isOrange && "infoBox__cases--green"
+          }`}
+        >
+          {cases}
+        </h2>
+        <Typography className="infoBox__total">{total} Total</Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default InfoBox
+export default InfoBox;
